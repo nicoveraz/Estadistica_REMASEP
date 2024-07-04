fe2937efb4d2159a1f8c8a47cc91d93c50572480
nicovera <nicovera@quetru.cl>
2024-07-04 23:21:08 +0000

    update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-document.getElementById("file-input").addEventListener("change", function() {
-    document.getElementById("analyze").disabled = !this.files.length;
-});
+window.onload = function() {
+    document.getElementById("analyze").disabled = true;
+};
+
+function fileSelected() {
+  document.getElementById("analyze").disabled = false;
+}
 
 function analyze(data) {
   // Your analysis code here
