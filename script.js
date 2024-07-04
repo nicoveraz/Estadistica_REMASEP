6aac43bd2b8b36aac5388f85a22eed743b20588f
nicovera <nicovera@quetru.cl>
2024-07-04 23:16:04 +0000

    updates

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.getElementById("file-input").addEventListener("change", function() {
+    document.getElementById("analyze").disabled = !this.files.length;
+});
+
 function analyze(data) {
   // Your analysis code here
   alert("Analysis complete!");
