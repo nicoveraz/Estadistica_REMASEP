68892ac3346c55ea22c11f6e751c6cd58183cc8b
nicovera <nicovera@quetru.cl>
2024-07-05 00:02:51 +0000

    test disable button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('fileInput').addEventListener('change', handleFileSelect);
-document.getElementById('button').disabled = true;
 
 let selectedFile;
 
@@ -9,11 +8,35 @@ function handleFileSelect(event) {
 
     // Check for .xlsx extension
     if (!fileName.endsWith('.xlsx')) {
-        alert('Por favor seleccione un archivo Excel con la extensión .xlsx');
+        alert('Por favor seleccione un archivo excel con la extensión .xlsx.');
         selectedFile = null;
+        processButton.disabled = true;
+        return;
     }
 
-    document.getElementById('button').disabled = false;
+    document.getElementById('selectedFileName').textContent = fileName;
+    processButton.disabled = false;
+    
+}
 
+function processFile() {
+    if (!selectedFile) {
+        alert('Por favor seleccione un archivo Excel');
+        return;
+    }
+    const reader = new FileReader();
 
+    reader.onload = function (event) {
+        const data = event.target.result;
+        const workbook = XLSX.read
+
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const data = XLSX.utils.sheet_to_json(sheet);
+
+        // Process the data here
+        console.log(data);
+    };
+
+    reader.readAsBinaryString(selectedFile);
 }
